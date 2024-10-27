8d03400c10ee6cf29e7c7c943f51e03ad9bb7d70
LikoKuridze <lkuridze@credo.ge>
2024-10-27 22:39:57 +0400

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+        int number;
+        string enteredNumbers = "";
+
+
+        Console.WriteLine("Enter positive numbers (enter a negative number to stop):");
+
+        while (true)
+        {
+            number = int.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                break;
+            }
+
+            sum += number;
+            enteredNumbers += number + "; ";
+
+        }
+        Console.WriteLine("Enter Numbers: " + enteredNumbers);
+        Console.WriteLine("Total: " + sum);
+
+    }
+}
